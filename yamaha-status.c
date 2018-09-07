2efabcd755345cacad18bf8e7a240b078e487720
Sven Fink <sven.fink@piclife.de>
2018-09-07 18:16:10 +0200

    Code Clean

--- yamaha-status.c
+++ yamaha-status.c
@@ -22,60 +22,76 @@
 
 #define GET_STATUS "/YamahaExtendedControl/v1/main/getStatus"
 
-int mapResponseToOuput(char* json, char* tag, char* expect, int output){
+int mapResponseToOuput(char *json, char *tag, char *expect, int output)
+{
   // search for "tag" on json response
   char *search = strstrskip(json, tag);
-  if(search == 0){
+  if (search == 0)
+  {
     setoutput(output, 0);
     return -1;
   }
 
-	int len = strlen(expect);
+  int len = strlen(expect);
   // check power value is "expect"
-  if(strncmp(search, expect, len) == 0){
+  if (strncmp(search, expect, len) == 0)
+  {
     setoutput(output, 1);
-  }else{
+  }
+  else
+  {
     setoutput(output, 0);
   }
 
   return 0;
 }
 
-
-while(true)
+while (true)
 {
   setoutput(OUT_STATUS, 1);
   sleep(PERIOD_MS);
 
-  char* ipAddress = getinputtext(INPUT_IP_ADDRESS);
+  char *ipAddress = getinputtext(INPUT_IP_ADDRESS);
 
-  char* response = httpget(ipAddress, GET_STATUS);
-  
-  if(response == 0){
+  setoutput(OUT_STATUS, 2);
+
+  char *response = httpget(ipAddress, GET_STATUS);
+
+  setoutput(OUT_STATUS, 3);
+
+  if (response == 0)
+  {
     setoutput(OUT_STATUS, 100);
     setoutput(OUT_POWER_ON, 0);
     setoutput(OUT_INPUT_SELECTED, 0);
     setoutput(OUT_IS_2CH, 0);
     sleeps(2);
-	continue;
+    continue;
   }
 
   int ret;
 
   ret = mapResponseToOuput(response, TAG_POWER, POWER_ON_TEXT, OUT_POWER_ON);
-  if(ret != 0){
+  if (ret != 0)
+  {
     setoutput(OUT_STATUS, 101);
     continue;
   }
 
+  setoutput(OUT_STATUS, 4);
+
   ret = mapResponseToOuput(response, TAG_SOUND_PROGRAM, IS_2CH_TEXT, OUT_IS_2CH);
-  if(ret != 0){
+  if (ret != 0)
+  {
     setoutput(OUT_STATUS, 102);
     continue;
   }
 
+  setoutput(OUT_STATUS, 5);
+
   ret = mapResponseToOuput(response, TAG_INPUT, HDMI2_SELECTED_TEXT, OUT_INPUT_SELECTED);
-  if(ret != 0){
+  if (ret != 0)
+  {
     setoutput(OUT_STATUS, 103);
     continue;
   }
